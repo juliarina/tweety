e19835ac7d3f00b4b920712b81ae9e4eda457bfe
Julia <rina.julianur@gmail.com>
2026-05-06 09:39:17 +0700

    change textarea placeholder in PostInput Component

--- src/components/PostInput.jsx
+++ src/components/PostInput.jsx
@@ -15,7 +15,7 @@ function PostInput({ addThread }) {
     <form className="post-input" onSubmit={onSubmitHandler}>
       <input className="post-input_title" type="text" placeholder="Title" value={title} onChange={onTitleChange} required/>
       <input className="post-input_category" type="text" placeholder="Category" value={category} onChange={onCategoryChange}/>
-      <textarea rows="4" className="post-input_description" type="text" value={body} onChange={onBodyChange} required/>
+      <textarea rows="4" className="post-input_description" placeholder="Write something ..." type="text" value={body} onChange={onBodyChange} required/>
       <button>Create</button>
     </form>
   );
