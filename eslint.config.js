e343c803d3bec3ea5e36186b65cd5f8034e6204d
Julia <rina.julianur@gmail.com>
2026-05-07 20:34:47 +0700

    install Storybook

--- eslint.config.js
+++ eslint.config.js
@@ -1,33 +1,25 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import globals from 'globals';
 import pluginJs from '@eslint/js';
 import pluginReact from 'eslint-plugin-react';
 import daStyle from 'eslint-config-dicodingacademy';
 import pluginCypress from 'eslint-plugin-cypress/flat';
 
-export default [
-  {
-    files: ['**/*.{js,mjs,cjs,jsx}'],
-  },
-
-  {
-    languageOptions: {
-      globals: {
-        ...globals.browser,
-        ...globals.node,
-      },
+export default [{
+  files: ['**/*.{js,mjs,cjs,jsx}'],
+}, {
+  languageOptions: {
+    globals: {
+      ...globals.browser,
+      ...globals.node,
     },
   },
-
-  pluginJs.configs.recommended,
-  pluginReact.configs.flat.recommended,
-  pluginCypress.configs.recommended,
-  daStyle,
-
-  {
-    settings: {
-      react: {
-        version: 'detect',
-      },
+}, pluginJs.configs.recommended, pluginReact.configs.flat.recommended, pluginCypress.configs.recommended, daStyle, {
+  settings: {
+    react: {
+      version: 'detect',
     },
   },
-];
+}, ...storybook.configs["flat/recommended"]];
