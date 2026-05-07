583a22816fbc280791cc750ce760a1cb242778f0
Julia <rina.julianur@gmail.com>
2026-05-07 20:15:24 +0700

    add login e2e test script

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,76 @@
+/**
+ * - Login spec
+ *   - should display login page correctly
+ *   - should display alert when email is empty
+ *   - should display alert when password is empty
+ *   - should display alert when email or password is wrong
+ *   - should display homepage when email and password is correct
+ */
+
+describe('Login spec', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:5173/');
+  });
+
+  it('should display login page correctly', () => {
+    // memverifikasi elemen yang harus tampak pada halaman login
+    cy.get('input[placeholder="Email"').should('be.visible');
+    cy.get('input[placeholder="Password"]').should('be.visible');
+    cy.get('button').contains(/^Login$/).should('be.visible');
+  });
+
+  it('should display alert when email is empty', () => {
+    // klik tombol login tanpa mengisi email
+    cy.get('button').contains(/^Login$/).click();
+
+    // memverifikasi window.alert untuk menampilkan pesan dari API
+    cy.on('window:alert', (str) => {
+      expect(str).to.equal('"email" is not allowed to be empty');
+    });
+  });
+
+  it('should display alert when password is empty', () => {
+    // mengisi email
+    cy.get('input[placeholder="Email"]').type('test@mail.com');
+
+    // klik tombol login tanpa mengisi password
+    cy.get('button').contains(/^Login$/).click();
+
+    // memverifikasi window.alert untuk menampilkan pesan dari API
+    cy.on('window:alert', (str) => {
+      expect(str).to.equal('"password" is not allowed to be empty');
+    });
+  });
+
+  it('should display alert when email or password is wrong', () => {
+    // mengisi email
+    cy.get('input[placeholder="Email"]').type('dicoding123@mail.com');
+
+    // mengisi password yang salah
+    cy.get('input[placeholder="Password"]').type('wrong_password')
+
+    // menekan tombol Login
+    cy.get('button').contains(/^Login$/).click();
+
+    // memverifikasi window.alert untuk menampilkan pesan dari API
+    cy.on('window:alert', (str) => {
+      expect(str).to.equal('email or password is wrong');
+    });
+  });
+
+  it('should display homepage when email and password is correct', () => {
+    // mengisi email
+    cy.get('input[placeholder="Email"]').type('dicoding123@mail.com');
+
+    // mengisi password yang salah
+    cy.get('input[placeholder="Password"]').type('dicoding123')
+
+    // menekan tombol Login
+    cy.get('button').contains(/^Login$/).click();
+
+    // memverifikasi bahwa elemen yang berada di homepage ditampilkan
+    cy.get('h3').contains('Popular Category').should('be.visible');
+    cy.get('h2').contains('Available Discussion').should('be.visible')
+    cy.get('.add-button').should('be.visible');
+  });
+});
