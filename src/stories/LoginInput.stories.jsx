b601b5be7b9b1eb3862ded26c75285b2c62403b7
Julia <rina.julianur@gmail.com>
2026-05-08 10:20:09 +0700

    add LoginInput stories component

---
+++ src/stories/LoginInput.stories.jsx
@@ -0,0 +1,14 @@
+import LoginInput from '../components/LoginInput';
+
+export default {
+  title: 'LoginInput',
+  component: LoginInput,
+};
+
+const TemplateStory = (args) => <LoginInput {...args} />
+
+export const Default = TemplateStory.bind({});
+
+Default.args = {
+  login: () => alert('login'),
+}
